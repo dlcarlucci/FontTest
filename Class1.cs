11e695bbe995fdafcbeadfd4f7ef82c56c0df609
Daniel Carlucci <daniel@danysoft.net>
2024-12-28 20:38:44 -0300

    Cleaning

---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace FontTest
+{
+    internal class Class1
+    {
+    }
+
+    class MyCon : IDisposable
+    {
+        SqlConnection Conn = null;
+
+        public MyCon()
+        {
+            Conn = new SqlConnection("");
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Conn.Close();
+            }
+        }
+
+        ~MyCon()
+        {
+            Dispose(false);
+        }
+    }
+}
